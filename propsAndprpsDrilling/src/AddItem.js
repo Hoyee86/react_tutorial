440b2343380f2d18596a527038b6890762f93797
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-26 15:13:50 +0100

    update

--- propsAndprpsDrilling/src/AddItem.js
+++ propsAndprpsDrilling/src/AddItem.js
@@ -18,7 +18,11 @@ const AddItem = ({ newItem, setNewItem, handleSubmit }) => {
         onChange={(e) => setNewItem(e.target.value)}
       />
 
-      <button type="submit" aria-label="Add Item">
+      <button type="submit" 
+      aria-label="Add Item"
+      onClick={(e) => inputRef.current.focus()}
+      >
+        
         <FaPlus />
       </button>
     </form>
