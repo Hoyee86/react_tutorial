fb163d69dcce141be846486c268e2e648626252e
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-26 12:01:48 +0100

    update

--- propsAndprpsDrilling/src/App.js
+++ propsAndprpsDrilling/src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
 import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
 
 
 
@@ -16,6 +17,7 @@ function App() {
   const [items, setItems] = useState(JSON.parse(localStorage.getItem("shoppinglist")))
   
   const [newItem, setNewItem] = useState('')
+  const [search, setSearch] = useState('')
   
   const setAndSaveItems = (newItems) => {
     setItems(newItems)
@@ -57,17 +59,27 @@ const addItem = (item) => {
   return (
     <div className="App">
       <Header title = "Welcome to Props👨‍💻"/>
+
+      <SearchItem
+      search = {search}
+      setSearch = {setSearch}
+      
+      />
+      
+     
       <AddItem
       newItem={newItem}
       setNewItem={setNewItem}
       handleSubmit={handleSubmit}
-      
       />
+
+      
       <Content 
         items={items}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
+      
       <Footer length={items.length}/>
     </div>
   );
--- propsAndprpsDrilling/src/SearchItem.js
+++ propsAndprpsDrilling/src/SearchItem.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const SearchItem = () => {
   return (
-   <form className="searchForm"></form>
+   <form className="searchForm">
+    <label htmlFor="search">Search</label>
+    <input 
+        id='search'
+        type='text'
+        role='searchbox'
+        placeholder='search Items'
+        />
+   </form>
   )
 }
 
