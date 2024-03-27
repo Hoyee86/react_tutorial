ba1a08327b0eb7fb85faaaa0e9ed15ab9c605692
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-27 11:27:45 +0100

    update

--- color-mod/src/Input.jsx
+++ color-mod/src/Input.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
-const Input = (
+const Input = ({
   colorValue,
   setColorValue,
   setHexValue,
   isDarkText,
   setIsDarkText
-) => {
+}) => {
   return (
     <form onClick={(e) => e.preventDefault()}>
-        <input type="text"  />
+      <input
+        type="text"
+        autoFocus
+        placeholder="Add Color Name"
+        required
+        value={colorValue}
+        onChange={(e) => 
+        setColorValue(e.target.value)
+        }
+
+      />
     </form>
   );
 };
