89dc6736424f89fa16e762f96351ba463b6872f5
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-02 15:49:54 +0100

    update

--- use-effect-challenge/src/App.js
+++ use-effect-challenge/src/App.js
@@ -1,12 +1,33 @@
-import { useState } from 'react';
-import './App.css';
+import { useState, useEffect } from 'react';
+
+import Form from './Form';
 
 function App() {
 
   const API_URL = 'https://jsonplaceholder.typicode.com/'
-  const [reqType, setREqType] = useState('users')
+  const [reqType, setReqType] = useState('users');
+  const [items, setItems] = useState([])
+
+    useEffect(() => {
+      const fetchItems = async() => {
+        try {
+          const response = await fetch (`${API_URL}${reqType}`);
+          const req = await response.json();
+          console.log(req)
+          setItems(req)
+
+        }catch (error) {
+          
+        }
+      }
+      fetchItems ();
+
+    }, [reqType])
+
+
   return (
     <div className="App">
+      <Form reqType = {reqType} setReqType={setReqType}/>
       
     </div>
   );
