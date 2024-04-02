d137e94476638f33e4466ae20cbf862bfeea0c75
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-02 14:56:28 +0100

    update

---
+++ use-effect-challenge/src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const API_URL = 'https://jsonplaceholder.typicode.com/'
+  const [reqType, setREqType] = useState('users')
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
