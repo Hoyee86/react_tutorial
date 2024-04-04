7a92922a8e3deae34d88aec8ae26d692f299eb59
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-04-04 11:15:03 +0100

    update

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,32 +1,64 @@
-import { Route, Routes } from "react-router-dom"
-import About from "./About"
-import Footer from "./Footer"
-import Header from "./Header"
-import Home from "./Home"
-import Missing from "./Missing"
-import Nav from "./Nav"
-import NewPost from "./NewPost"
-import Postpage from "./PostPage"
-
-
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import Nav from "./Nav";
+import NewPost from "./NewPost";
+import Postpage from "./PostPage";
+import { useState } from "react";
 
 function App() {
+  const [post, setPost] = useState([
+    {
+      id: 1,
+      title: "My first Post",
+      dateTime: "April 04, 2024 11:01:34 AM",
+      body: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, ad omnis iusto exercitationem nisi laudantium soluta nihil quia optio commodi neque dolores voluptas officia dolore aspernatur libero, quaerat recusandae inventore!",
+    },
+    {
+      id: 2,
+      title: "My Second Post",
+      dateTime: "April 04, 2024 11:01:34 AM",
+      body: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, ad omnis iusto exercitationem nisi laudantium soluta nihil quia optio commodi neque dolores voluptas officia dolore aspernatur libero, quaerat recusandae inventore!",
+    },
+    {
+      id: 3,
+      title: "My third Post",
+      dateTime: "April 04, 2024 11:01:34 AM",
+      body: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, ad omnis iusto exercitationem nisi laudantium soluta nihil quia optio commodi neque dolores voluptas officia dolore aspernatur libero, quaerat recusandae inventore!",
+    },
+    {
+      id: 4,
+      title: "My fourth Post",
+      dateTime: "April 04, 2024 11:01:34 AM",
+      body: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, ad omnis iusto exercitationem nisi laudantium soluta nihil quia optio commodi neque dolores voluptas officia dolore aspernatur libero, quaerat recusandae inventore!",
+    },
+    {
+      id: 5,
+      title: "My fifth Post",
+      dateTime: "April 04, 2024 11:01:34 AM",
+      body: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, ad omnis iusto exercitationem nisi laudantium soluta nihil quia optio commodi neque dolores voluptas officia dolore aspernatur libero, quaerat recusandae inventore!",
+    },
+  ]);
+
+  const [search, setSearch] = useState("");
 
   return (
     <div className="App">
       <Header title="DLT Blogs" />
       <Nav />
       <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/post" element={<NewPost/>} />
-      <Route path="/post/:id" element={<Postpage/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="*" element={<Missing/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<NewPost />} />
+        <Route path="/post/:id" element={<Postpage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
       </Routes>
       <Footer />
-    
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
